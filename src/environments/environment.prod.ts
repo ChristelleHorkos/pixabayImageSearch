fcb53ee40354333ddb2a1744c7bf3cb8747bd31c
Christelle Horkos <christellehorkos22@gmail.com>
2021-12-04 15:30:34 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  PIXABAY_API_KEY :"24573626-6c150bc3fd30a6b471347d682" ,
+  PIXABAY_API_URL:"https://pixabay.com/api/?key="
 };
